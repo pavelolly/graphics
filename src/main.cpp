1523428b3b39275ca6ca35a918bb52db2a80774e
km <lvanfomin069@gmail.com>
2025-03-23 12:14:32 +0300

    better scene switching

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #define WIDTH  1600
 #define HEIGHT 900
 
+void DrawThePlayground();
+
 int main() {
     InitWindow(WIDTH, HEIGHT, "Graphics");
     SetTargetFPS(240);
@@ -32,33 +34,32 @@ int main() {
             if (scene) {
                 scene->Draw();
             } else {
-
-                DrawLineBezier({100, 100}, {200, 200}, 2, YELLOW);
-                GuiGroupBox({300, 300, 400, 400}, "Window"); 
-
-                DrawLineDotted({450, 450}, {500, 500}, 20, 3, GRAY);
-                DrawCircleV({450, 450}, 3, RED);
-                DrawCircleV({500, 500}, 3, RED);
+                DrawThePlayground();
             }
 
         EndDrawing();
 
         // scene is not switchable when input boxes are active
         if (!scene || scene->IsSwitchable()) {
-            if (IsKeyPressed('1')) {
-                EnableCursor();
+            switch (GetCharPressed()) {
+            case '1':
+                ShowCursor(); 
                 scene = &scene_ellipses;
-            } else if (IsKeyPressed('2')) {
-                EnableCursor();
+                break;
+            case '2':
+                ShowCursor(); 
                 scene = &scene_draw_polygons;
-            } else if (IsKeyPressed('3')) {
-                DisableCursor();
+                break;
+            case '3':
+                HideCursor();
                 scene = &scene_localization;
-            } else if (IsKeyPressed('4')) {
-                EnableCursor();
+                break;
+            case '4':
+                ShowCursor(); 
                 scene = &scene_elementary_bezier;
-            } else if (IsKeyPressed('0')) {
-                EnableCursor();
+                break;
+            case '0':
+                ShowCursor(); 
                 scene = nullptr; // empty scene used for testing
             }
         }
@@ -71,4 +72,13 @@ int main() {
     CloseWindow();
 
     return 0;
+}
+
+void DrawThePlayground() {
+    DrawLineBezier({100, 100}, {200, 200}, 2, YELLOW);
+    GuiGroupBox({300, 300, 400, 400}, "Window"); 
+
+    DrawLineDotted({450, 450}, {500, 500}, 20, 3, GRAY);
+    DrawCircleV({450, 450}, 3, RED);
+    DrawCircleV({500, 500}, 3, RED);
 }
